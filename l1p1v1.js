2ffde3930440f3d314e63d1d750b01ff1d604a25
Kapitoshka1337 <Artpolevtsov63@mail.ru>
2022-09-29 21:12:20 +0300

    before add class

--- l1p1v1.js
+++ l1p1v1.js
@@ -1,16 +1,38 @@
 const PERMISSIONS_BAN = 0o000;
 const PERMISSIONS_ALLOW = 0o777;
 
+class files {
+    constructor(filePath) {
+        this.filePath = filePath;
+        this.permissions = 0;
+    }
+    access_permissions = () => {
+        this.permissions = 1;
+    }
+    deny_permissions = () => {
+        this.permissions = 0;
+    }
+    get permissions() {
+        return this.permissions;
+    }
+    get filePath(){
+        return this.filePath;
+    }
+
+    permissions;
+    filePath;
+}
+
 const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 const readline = require("readline").createInterface({input: process.stdin, output: process.stdout})
 
 const template = path.resolve(__dirname, "template.tbl");
-const fileNames = fs.readFileSync(template, 'utf-8').split('\n');
+const fileData = fs.readFileSync(template, 'utf-8').split('\n');
 
 let password_flag = 0;
-const cipher = fileNames.shift();
+const cipher = fileData.shift();
 const filePaths = fs.readFileSync(template, 'utf-8').split('\n').map(file=>path.resolve(__dirname, file));
 
 const key = Buffer.from('xNRxA48aNYd33PXaODSutRNFyCu4cAe/InKT/Rx+bw0=', 'base64');
@@ -45,34 +67,32 @@ const allow_access = (filePaths) => {
     }
 };
 
-deny_access(filePaths);
+deny_access(filePaths); //Deny access for all files
 
-readline.question("Your password: ", user_password => {
-    if(user_password == decrypt(algorithm, key, iv, cipher)) {
+readline.question("Your password: ", user_password => { //Read password from console
+    if (user_password == decrypt(algorithm, key, iv, cipher)) {
         password_flag = 1;
         allow_access(filePaths);
     }
-    fs.watch(path.resolve(__dirname), (event, filename)=>{
+    fs.watch(path.resolve(__dirname), (event, filename) => {
         console.log(event);
         switch (event) {
-            case "rename":
-            {
+            case "rename": {
                 console.log("we at case rename");
-                if(fileNames.indexOf(filename) != -1){
-                    if(password_flag) allow_access({filename});
+                if (fileNames.indexOf(filename) != -1) {
+                    if (password_flag) allow_access({filename});
                     else deny_access({filename});
                 }
             }
-            case "change":
-            {
+            case "change": {
                 console.log("we at case change");
-                if(fileNames.indexOf(filename) != -1)
-                    if(!password_flag) deny_access({filename})
+                if (fileNames.indexOf(filename) != -1)
+                    if (!password_flag) deny_access({filename})
             }
         }
 
     })
-    fs.watch(template, (event, filename)=>{
+    fs.watch(template, (event, filename) => {
         /*if(event){
             filePaths = filePathsCreator(template);
 
@@ -80,7 +100,8 @@ readline.question("Your password: ", user_password => {
             else deny_access(filePaths);
 
             console.log(filePaths)
-        }*/
+        }
+    })*/
+        readline.close();
     })
-    readline.close();
 })
